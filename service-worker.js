7aa80dbcb20c4480fd63a2b3e891d7433f2b4b05
tz8xdy75xh-ship-it <tz8xdy75xh@privaterelay.appleid.com>
2025-10-30 05:59:01 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('cs-v1').then(cache => cache.addAll([
-    '/', '/index.html', '/styles.css', '/app.js', '/terms.html'
-  ])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
